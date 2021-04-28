8f83b7bf4b4504ff00ed4e9bbb5fd110a886b7a8
Andres Ramos <arramos007@gmail.com>
2021-04-27 20:26:51 -0500

    Commit Exámen

---
+++ AndresRamosExamen/Login.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace AndresRamosExamen
+{
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnButtonClicked(object sender, EventArgs args)
+        {
+          
+            if (txtUsuario.Text != "estudiante2021" && txtPassword.Text != "uisrael2021")
+            {
+                await Navigation.PushAsync(new Registro(txtUsuario.Text, txtPassword.Text));
+            }
+            else
+            {
+                await DisplayAlert("Mensaje de bienvenida", "El usuario " + txtUsuario.Text + " es incorrecto!", "Aceptar");
+            }
+            
+        }
+    }
+}
---
+++ AndresRamosExamen/Registro.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AndresRamosExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        String usuario;
+        double totalCurso = 0;
+        public Registro(String user, String password)
+        {
+            usuario = user;            
+            InitializeComponent();
+        }
+
+        private void OnButtonClickedCalcular(object sender, EventArgs args)
+        {
+            int curso = 1800;
+
+            double cuota = (( curso - double.Parse("0" + txtMontoInicial.Text)) /3 ) + (1800 * 0.05);
+
+            txtPagoMensual.Text = cuota.ToString();
+
+            totalCurso = double.Parse(txtMontoInicial.Text) + (cuota * 3);
+        }
+
+        private async void OnButtonClickedGrabar(object sender, EventArgs args)
+        {
+            await DisplayAlert("Mensaje Información", "El grabado con éxito!", "Aceptar");
+            await Navigation.PushAsync(new Resumen(usuario, txtNombre.Text,  totalCurso.ToString()));
+        }
+    }
+}
